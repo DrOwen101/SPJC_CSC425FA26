8b32cf9286c671631aacc4e451f9ad3912aee577
DrOwen101 <151655844+DrOwen101@users.noreply.github.com>
2026-08-25 14:26:34 -0500

    Updating colorpallet to SPJC colors: JTO

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import SPJCLogo from './assets/SPJCLogo.jpg'
 import './App.css'
 
