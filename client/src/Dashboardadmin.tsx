4243125205f936c96e09a4733b5e92ce49ef94c2
Graham Vittetoe <grahamvittetoe13@gmail.com>
2026-09-03 15:38:09 -0500

    Lab 3 WIP
    fixed 3 bugs in App.tsx
    Updated Admin Dashboard Layout

--- client/src/Dashboardadmin.tsx
+++ client/src/Dashboardadmin.tsx
@@ -3,14 +3,17 @@ function Dashboardadmin() {
     <>
       <header>
         <h1>Admin Dashboard</h1>
-        <p>University Administration Portal</p>
+        <p style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold'}}>University Administration Portal</p>
 
-        <nav aria-label="Admin navigation">
+          <nav style={{ textAlign: 'center', fontSize: '1.7em', fontWeight: 'bold', lineHeight: '3', textDecoration: 'none', color: 'blue' }} aria-label="Admin navigation">
           <a href="#user-management">User Management</a>
+          <br/>
           <a href="#course-management">Course Management</a>
+          <br/>
           <a href="#reports">Reports</a>
+          <br/>
           <a href="#admin-alerts">Alerts</a>
-          <a href="#system-status">System Status</a>
+          <br/><a href="#system-status">System Status</a>
         </nav>
       </header>
 
