f998349efd6b86b77e7f4999cc6cf43c671fa1cb
Graham Vittetoe <grahamvittetoe13@gmail.com>
2026-09-06 13:15:19 -0500

    Module 3 Lab work Completed
    Adjusted Student, Admin, & Faculty dashboard text layouts, added SPJC logo to each dashboard.

--- client/src/Dashboardadmin.tsx
+++ client/src/Dashboardadmin.tsx
@@ -1,10 +1,11 @@
+import SPJCLogo from './assets/SPJCLogo.jpg'
 function Dashboardadmin() {
   return (
     <>
       <header>
         <h1>Admin Dashboard</h1>
+        <img src={SPJCLogo} alt="SPJC Logo" style={{ width: '100px', height: 'auto' }} />
         <p style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold'}}>University Administration Portal</p>
-
           <nav style={{ textAlign: 'center', fontSize: '1.7em', fontWeight: 'bold', lineHeight: '3', textDecoration: 'none', color: 'blue' }} aria-label="Admin navigation">
           <a href="#user-management">User Management</a>
           <br/>
