a7f33cd28618b6c3f4430311e7119d80d8cdc69a
java-VIP-Ben <2smallgarage@gmail.com>
2026-09-05 21:01:31 -0500

    most recent commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
           <img src={SPJCLogo} className="base" width="170" height="179" alt="SPJC logo" />
         </div>
         <div>
-          <h1>BEN LUKENS BRANCH WOOHOO</h1>
+          <h1>BEN LUKENS BRANCH</h1>
           <h1>SPJC</h1> {/* Change to College Name */}
           <p>Edit <code>src/App.tsx</code> and save to test <code>HMR</code></p>
         </div>
